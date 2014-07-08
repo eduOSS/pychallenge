c1d56f53b7d4c96265248a65bb412679f69742b3
leo.adams <leo.adams.zhang@gmail.com>
2014-07-08 20:11:07 +0800

    solve the comment problem

--- 2_pra_1.py
+++ 2_pra_1.py
@@ -2,13 +2,13 @@ from bs4 import BeautifulSoup, Comment
 import urllib2
 url = "http://www.pythonchallenge.com/pc/def/ocr.html"
 html = urllib2.urlopen(url).read()
-print html
+html ='<html><body>'+html+'</html></body>'
+#print html
 soup = BeautifulSoup(html)
 
 f = lambda text:isinstance(text,Comment)
 
 comments = soup.find_all(text = lambda text:isinstance(text,Comment))
-print comments
 for comment in comments:
     comment.extract()
     print comment
