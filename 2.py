5389827f6be5ddc814d60cfe960c6f9ac925392c
leo.adams <leo.adams.zhang@gmail.com>
2014-07-10 16:39:20 +0800

    update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
+from string import letters
 from bs4 import BeautifulSoup, Comment
-import urllib2,collections
+import urllib2,collections,string,re
 url = "http://www.pythonchallenge.com/pc/def/ocr.html"
 html = urllib2.urlopen(url).read()
 html ='<html><body>'+html+'</html></body>'
@@ -12,12 +13,22 @@ comments = soup.find_all(text = lambda text:isinstance(text,Comment))
 #for comment in comments:
 #    comment.extract()
 #    print comment
-d = collections.defaultdict(int)
-for c in comments[1]:
-    d[c] += 1
-    #print d[c]
+#d = collections.defaultdict(int)
+#for c in comments[1]:
+#    d[c] += 1
+#    #print d[c]
 #print d[0].__class__
-for c in sorted(d, key = d.get,reverse = True):
-    print c,d[c]
-    #print '$s %d' % (c,d[c])
+#for c in sorted(d, key = d.get,reverse = True):
+#    #print c,d[c]
+#    print '%s %d' % (c,d[c])
 #typeError: %d format: a number is required, not unicode 
+text = comments[1]
+#print filter(lambda x: x in string.letters, text)
+#print re.findall(r'[a-z]',text)
+#print ''.join([x for x in text if x in letters])
+#textlist = set(text)
+#for i in textlist:
+#    print i, text.count(i)
+for ch in text:
+    if ch.isalpha():
+        print ch
--- 2_pra_1.py
+++ 2_pra_1.py
@@ -1,5 +1,5 @@
 from bs4 import BeautifulSoup, Comment
-import urllib2
+import urllib2,collections
 url = "http://www.pythonchallenge.com/pc/def/ocr.html"
 html = urllib2.urlopen(url).read()
 html ='<html><body>'+html+'</html></body>'
@@ -9,6 +9,16 @@ soup = BeautifulSoup(html)
 f = lambda text:isinstance(text,Comment)
 
 comments = soup.find_all(text = lambda text:isinstance(text,Comment))
-for comment in comments:
-    comment.extract()
-    print comment
+#for comment in comments:
+#    comment.extract()
+#    print comment
+d = collections.defaultdict(int)
+for c in comments[1]:
+    d[c] += 1
+    #print type(d[c]) #all outcomes are int 
+#print d[0].__class__ # the result is int
+for c in sorted(d, key = d.get,reverse = True):
+    print c,d[c]
+    #print '$s %d' % (c,int(d[c]))
+    #TypeError: %d format: a number is required, not unicode
+    # there is no reason!
