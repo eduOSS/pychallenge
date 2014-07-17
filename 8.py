fd01210aebd983e41e70f95790d330e8074a18fa
leo.adams <leo.adams.zhang@gmail.com>
2014-07-17 20:59:54 +0800

    quick_flush

---
+++ 8.py
@@ -0,0 +1,11 @@
+#references:http://stackoverflow.com/questions/7784148/understanding-repr-function-in-python/7784214#7784214
+#http://effbot.org/librarybook/bz2.htm
+#
+#!/usr/bin/python
+#coding:utf8
+
+__author__ = ['leo.adams']
+
+import urllib2,re,bz2
+un,pw =[bz2.decompress(eval(i)) for i in re.findall(r'(\'.+\')',urllib2.urlopen('http://www.pythonchallenge.com/pc/def/integrity.html').read())]
+print un,pw
--- 8_pra_0.py
+++ 8_pra_0.py
@@ -1,9 +1,18 @@
-#references:
+#references:http://stackoverflow.com/questions/7784148/understanding-repr-function-in-python/7784214#7784214
+#http://effbot.org/librarybook/bz2.htm
 #
 #!/usr/bin/python
 #coding:utf8
 
 __author__ = ['leo.adams']
 
-
-
+import urllib2,re,bz2
+url = 'http://www.pythonchallenge.com/pc/def/integrity.html'
+html = urllib2.urlopen(url).read()
+un,pw =[bz2.decompress(eval(i)) for i in re.findall(r'(\'.+\')',html)]
+#l = "the meaning of life" 
+#l1 = bz2.compress(l)
+#l2 = bz2.decompress(l1)
+#print repr(l1),'\n',l2
+print un,pw
+#print repr(un),repr(pw)
