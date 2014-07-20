8995c69d3ba54938c7324ecbd9a60628ff204ea4
leo.adams <leo.adams.zhang@gmail.com>
2014-07-20 13:15:21 +0800

    get_the_9

--- 8_pra_0.py
+++ 8_pra_0.py
@@ -16,3 +16,8 @@ un,pw =[bz2.decompress(eval(i)) for i in re.findall(r'(\'.+\')',html)]
 #print repr(l1),'\n',l2
 print un,pw
 #print repr(un),repr(pw)
+un, pw = re.findall(r'(\'.+\')',html)
+print type(eval(un)),'**',type(pw)
+print eval(un).decode('bz2'),eval(pw).decode('bz2')
+#print '*********'
+#print eval("BZh91AY&SY\x94$|\x0e\x00\x00\x00\x81\x00\x03$ \x00!\x9ah3M\x13<]\xc9\x14\xe1BBP\x91\xf08").encode('bz2')
--- 9_pra_0.py
+++ 9_pra_0.py
@@ -1,9 +1,75 @@
-#references:
-#
+#references:http://inteletec.com/python-http-url-request-of-password-protected-remote-server-folder/
+#https://gist.github.com/nakamura001/3224974
 #!/usr/bin/python
 #coding:utf8
 
 __author__ = ['leo.adams']
 
 
+import urllib2,requests,re,StringIO,urllib,glob
+from PIL import Image,ImageDraw
+from bs4 import BeautifulSoup,Comment
+from StringIO import StringIO
+username = 'huge'
+password = 'file'
+url = 'http://www.pythonchallenge.com/pc/return/good.html'
+img_url = 'http://www.pythonchallenge.com/pc/return/good.jpg'
+r = requests.get(url, auth=(username, password))
+#html = urllib2.urlopen(url).read()
+html = r.content 
+soup = BeautifulSoup('<html><body>'+html+'</html></body>')
+comment = soup.find_all(text = lambda text: isinstance(text,Comment))[1]
+#num_list = re.split(':|,',comment.replace('\n','').replace('second:',','))[1:]
+#num = re.findall(r'first:(\d+)\n\n',html)
+#print len(num_list)
+nouse, first, second = comment.replace('second','').replace('-->','').replace('\n','').split(':')
+#print first,'\n\n', second
+first = [int(i) for i in ''.join(first).split(',')]
+second = [int(i) for i in ''.join(second).split(',')]
+#coords = zip(first,second)
 
+name = 'base.jpg'
+if not glob.glob(name):
+    r_img = requests.get(img_url, auth=(username, password))
+    #im = Image.open(StringIO(r_img.content))
+    f = open(name,'wb')
+    f.write(r_img.content)
+    f.close()
+
+
+#Image.open(name).save('base.png')
+#f = Image.open('base.png')
+f = Image.new('RGB',(500,500),'white')
+draw = ImageDraw.Draw(f)
+draw.line(first,'red',2)
+draw.line(second,'red',2)
+#draw.polygon(coords,outline = 'black')
+#draw.line(coords,width = 5)
+i = iter(first)
+for k in range(len(first)/2):
+    #p=getPixel(f,coords[i][0],coords[i][1])
+    #p=f.getPixel(coords[i][0],coords[i][1])
+    draw.point((i.next(),i.next()),fill = 'black')
+
+j = iter(second)
+for k in range(len(second)/2):
+    #p=getPixel(f,coords[i][0],coords[i][1])
+    #p=f.getPixel(coords[i][0],coords[i][1])
+    draw.point((j.next(),j.next()),fill = 'black')
+f.save('base1.png')
+f.close()
+#f.save('file.png')    
+#im.save(name,'JPEG')
+#print im.getdata()
+#print comment
+#im = requests.get(jpg_url,auth=(username,password))
+#print im
+#value = {
+#        'username': username,
+#        'password': password
+#        }
+#data = urllib.urlencode(value)
+#req = urllib2.Request(jpg_url,data)
+#response = urllib2.urlopen(req)
+#im = StringIO.StringIO(response.read())
+#print im.info
